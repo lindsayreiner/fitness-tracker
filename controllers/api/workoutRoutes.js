c7e6ad9f7ecb28d4faf28ae046ebb35dbead9fbe
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-12-10 21:24:37 -0500

    pushing git current to deploy to heroku

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
-const { Workout } = require('../../models')
+const Workout = require('../../models/workout');
 
-router.get('/workouts', async (req, res) => {
-
+router.get('/workouts', async ({ body }, res) => {
+    await db.getLastInsertedDocument.find({}).sort({ _id: -1 }).limit(1)
 });
+
+// router.post('/workouts', async ({ body }, res) => {
+
+// })
+
+// router.post()
 
 module.exports = router;
